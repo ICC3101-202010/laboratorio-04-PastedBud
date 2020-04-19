49db4b597ede0f77639a11f3325e705c70c3abb9
Carlo Vitali <Carlo@Air-de-Carlos.iptv.microsoft.com>
2020-04-19 10:03:20 -0400

    first commit

---
+++ Lab4Vitali/Lab4Vitali/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab4Vitali
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
